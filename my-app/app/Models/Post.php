f7da0c4742e51febd1923af5cc2d7385d6031908
yukin <yukinnh19950123@gmail.com>
2022-09-25 15:59:21 +0900

    構築①

---
+++ my-app/app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+}
--- my-app/app/Providers/AppServiceProvider.php
+++ my-app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 動画
+        Paginator::useBootstrap(); 
+
+
+        // Paginator::useBootstrapFive();    　公式ドキュメント
+        //または Paginator::useBootstrapFour();　　   公式ドキュメント
     }
 }
