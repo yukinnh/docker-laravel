8c6339e96e1f2200bb18544921d59d166786acfd
yukin <yukinnh19950123@gmail.com>
2022-10-08 13:36:27 +0900

    Revert "dddd"
    This reverts commit fd9f5589328af1c94eb492541881f17f4bc56193.

---
+++ my-app/resources/views/posts/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="create">
+            <a href="{{ route('posts.create') }}">投稿作成</a>
+        </div>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>
+                        <a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a>
+                    </h2>
+                    <a href="{{ route('categories.index', ['category' => $post->category_id]) }}">{{ $post->category->name }}</a>
+                    <p class='body'>{{ $post->body }}</p>
+                    <form action="{{ route('posts.delete', ['post' => $post->id]) }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+    <script>
+        function deletePost(id) {
+            'use strict'
+
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
+            }
+        }
+    </script>
+</html>
